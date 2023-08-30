6335abe023c0a9130b72450382a10eba54702df4
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-30 15:07:39 +0500

    Update adaptive_design.py

--- Mobile_Responsiveness/adaptive_design.py
+++ Mobile_Responsiveness/adaptive_design.py
@@ -1,6 +1,60 @@
-def adapt_to_screen_size(screen_size):
-    """
-    Adjust the application design based on the screen size.
-    """
-    # Logic to adapt the UI elements based on screen size
-    pass
+# Mock functions to simulate device and orientation detection
+def detect_device_type():
+    # This is a placeholder. In a real-world scenario, you'd use a library or framework to detect the device type.
+    return "desktop"  # Possible values: "mobile", "tablet", "desktop"
+
+def detect_orientation():
+    # This is a placeholder. In a real-world scenario, you'd use device sensors or screen width/height ratio.
+    return "landscape"  # Possible values: "portrait", "landscape"
+
+
+class AdaptiveDesign:
+
+    def __init__(self):
+        self.device_type = detect_device_type()
+        self.orientation = detect_orientation()
+        self.theme = "light"  # Default theme
+
+    def adapt_to_screen_size(self):
+        """
+        Adjust the application design based on the screen size.
+        """
+        container_width, container_padding, font_size = 1000, 20, 16  # Default values
+
+        if self.device_type == "mobile":
+            container_width = 600
+            container_padding = 10
+            font_size = 14
+        elif self.device_type == "tablet":
+            container_width = 800
+            container_padding = 15
+            font_size = 15
+
+        # Adjustments for orientation
+        if self.orientation == "portrait":
+            font_size -= 1
+
+        print(f"Set container width to: {container_width}px")
+        print(f"Set container padding to: {container_padding}px")
+        print(f"Set font size to: {font_size}px")
+
+    def toggle_theme(self):
+        """
+        Toggle between dark and light mode.
+        """
+        if self.theme == "light":
+            self.theme = "dark"
+        else:
+            self.theme = "light"
+        print(f"Theme set to: {self.theme}")
+
+    def get_current_settings(self):
+        """
+        Get the current adaptive settings.
+        """
+        return {
+            "device_type": self.device_type,
+            "orientation": self.orientation,
+            "theme": self.theme
+        }
+
